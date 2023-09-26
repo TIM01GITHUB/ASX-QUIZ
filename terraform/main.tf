7dd33829fe2eae7bab12e0904f762accabaef848
Aryabhata <142987366+TIM01GITHUB@users.noreply.github.com>
2023-09-27 06:24:29 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Defining a Google Instance and its attributes
 resource "google_compute_instance" "vm" {
     project = var.project_id
     name = var.instance_name
@@ -5,8 +6,15 @@ resource "google_compute_instance" "vm" {
     machine_type = var.machine_type
     zone = var.zone
     hostname = var.hostname
-	
+/* 
+a) Dynamic block definition for configuring the service account for the instance.
+b) Using for each to conditionally include  the service_account block.
+c) Setting up the email of the service account from the input variable - var.service_account
+d) Setting up the scope for the service account from the input variable - var.scopes 
+*/ 
+
     dynamic service_account {
+
         for_each = var.service_account != "" ? [1] : []
         content {
             email = var.service_account
